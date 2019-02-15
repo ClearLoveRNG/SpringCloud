61d0eaf42df4d566c661ec8a4a8f9e14d36b1c6e
jianghaotian <jiang.haotian@trs.com.cn>
2019-02-15 18:03:41 +0800

    SpringCloudConfig

--- config-client/src/main/java/com/example/configclient/controller/ConfigClientController.java
+++ config-client/src/main/java/com/example/configclient/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.example.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Author: jianghaotian
  * Create Time:2019/2/15 15:08
  */
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
